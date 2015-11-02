f83e77544aac67b610b74457a4da00e2457a211e
neha-2-r <rathi@u.northwestern.edu>
2015-11-01 20:45:30 -0600

    worked on delete

--- grocery_aqua/app/example/scripts/GettingStartedController.js
+++ grocery_aqua/app/example/scripts/GettingStartedController.js
@@ -4,14 +4,22 @@ angular
     $scope.navbarTitle = "Groceries";
     var items = [
       { 
+        id: 0,
         name: 'Bananas',
         quantity: 6
       },
       {
+        id: 1,
         name: 'Apples',
         quantity: '3'
       }
   	];
   	$scope.formData = {};
   	$scope.formData.items = items; 
+    $scope.delete = function(id)
+    {
+      supersonic.logger.log("delete clicked, id = " + id); 
+      $("#" + id).css("color", "red"); 
+
+    }  
   });
