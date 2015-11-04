0dca39d6ac055485c5f591d393ae3e64b91be5ef
brendanfrick2016 <brendanfrick2016@u.northwestern.edu>
2015-11-04 12:00:07 -0600

    Fixed submit button, fixed unit list

--- grocery_aqua/app/example/scripts/UploadController.js
+++ grocery_aqua/app/example/scripts/UploadController.js
@@ -36,6 +36,18 @@ angular
                 message: "Item Submitted!",
                 buttonLabel: "Done!"
             };
+            var image = document.getElementById('showImage');
+            image.src = "/placeholder.png";
+            var item_name = document.getElementById('item_name');
+            item_name.value ="";
+            var item_quantity = document.getElementById('item_quantity');
+            item_quantity.value = 0;
+            var item_unit = document.getElementById('item_unit');
+            item_unit.value ="";
+            var item_info = document.getElementById('item_info');
+            item_info.value ="";
+
+
 
             supersonic.ui.dialog.alert("Success", instructions).then(function() {
 
