90aea6c7c42ec957f29cb45e4eef9b3a424dc5e6
sujana <sujana1290@gmail.com>
2015-11-10 19:26:17 -0600

    Retrieving images from Database

--- app/example/scripts/GettingStartedController.js
+++ app/example/scripts/GettingStartedController.js
@@ -1,7 +1,83 @@
 angular
   .module('example')
   .controller('GettingStartedController', function($scope, supersonic) {
-    $scope.navbarTitle = "Groceries";
+  $scope.resultImages = [];
+$scope.resultImages1 = [];
+
+
+
+$scope.current= function() {
+    var imageClass= Parse.Object.extend("ImageData");
+    var imgQuery = new Parse.Query(imageClass);
+    imgQuery.equalTo("item_status","O")
+
+      imgQuery.find({
+      success: function(results) {
+          
+        // supersonic.ui.dialog.alert(results.length);
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results.length; i++) {
+ 
+          var object = results[i];
+          var newImage = {};
+          newImage.name = object.get("item_name");
+
+          newImage.quantity = object.get("item_quantity");
+          newImage.unit = object.get("item_unit");
+          newImage.info = object.get("item_info");
+         newImage.id = object.id;
+       supersonic.logger.log(newImage.id);
+          var image = object.get("itemImage");
+          newImage.photo = image.url();
+        
+          $scope.resultImages.push(newImage);
+         
+        }
+      },
+      error: function(error) {
+        supersonic.ui.dialog.alert('Not Working!!');
+      }
+    });
+    }
+
+
+$scope.previous= function() {
+
+    var prevClass= Parse.Object.extend("ImageData");
+    var prevQuery = new Parse.Query(prevClass);
+    prevQuery.equalTo("item_status","P")
+
+      prevQuery.find({
+      success: function(results1) {
+          
+        // supersonic.ui.dialog.alert(results.length);
+        // Do something with the returned Parse.Object values
+        for (var i = 0; i < results1.length; i++) {
+ 
+          var object = results1[i];
+          var newImage1 = {};
+          newImage1.name = object.get("item_name");
+
+          newImage1.quantity = object.get("item_quantity");
+          newImage1.unit = object.get("item_unit");
+          newImage1.info = object.get("item_info");
+         newImage1.id = object.id;
+       supersonic.logger.log(newImage1.id);
+          var image = object.get("itemImage");
+          newImage1.photo = image.url();
+          $scope.resultImages1.push(newImage1);
+          supersonic.logger.log($scope.resultImages1[i]);
+         
+        }
+      },
+      error: function(error) {
+        supersonic.ui.dialog.alert('Not Working!!');
+      }
+    });
+    }
+
+
+     $scope.navbarTitle = "Groceries";
     $scope.swipeID = -1;
     $scope.isSwipeID = function(id) {
       if (swipeID == id) {
@@ -10,7 +86,10 @@ angular
       return false;
     }
     $scope.state = "NORMAL";
-    $scope.groceryItems = [
+
+    $scope.current();
+       $scope.previous();
+    /*$scope.groceryItems = [
       {
         id: 0,
         name: 'Bananas',
@@ -32,61 +111,59 @@ angular
         unit: 'unit(s)',
         onList:false      }
   	];
-
+*/
     $scope.delete = function(id) {
 
       supersonic.logger.log("delete clicked, id = " + id); 
       
-      for (var i = 0; i < $scope.groceryItems.length; i++) {
-        if ($scope.groceryItems[i].id == id) {
-          $scope.groceryItems.splice(i, 1);
-        }
-      }
-      swipeID = -1;
 
     }
 
-    $scope.open = function(id) {
+    $scope.open = function(itemID) {
 
-      supersonic.logger.log("delete clicked, id = " + id); 
-      var name = "";
-      for (var i = 0; i < $scope.groceryItems.length; i++) {
-        if ($scope.groceryItems[i].id == id) {
-          $scope.groceryItems[i].onList = true;
-          name = $scope.groceryItems[i].name;
-        }
+    var  UpdateClass= Parse.Object.extend("ImageData");
+            var status="O";
+   var updateQuery = new UpdateClass();
+   updateQuery.id=itemID;
+   updateQuery.set("item_status",status);
+   updateQuery.save(null,{
+      success: function(updateQuery) {
+
+supersonic.logger.log("saved successfully");
+          
+ 
+      },
+      error: function(updateQuery,error) {
+        supersonic.ui.dialog.alert('Not Working!!');
       }
-      swipeID = -1;
+    });
 
-      var instructions = {
-                message: name,
-                buttonLabel: "Ok"
-            };
-
-            supersonic.ui.dialog.alert("Item added:", instructions).then(function() {
-
-            });
+  $scope.current();
 
     }
-    $scope.close = function(id) {
 
-      supersonic.logger.log("delete clicked, id = " + id); 
-      
-      var name = "";
-      for (var i = 0; i < $scope.groceryItems.length; i++) {
-        if ($scope.groceryItems[i].id == id) {
-          $scope.groceryItems[i].onList = false;
-          name = $scope.groceryItems[i].name;
-        }
+
+
+    $scope.close = function(itemID) {
+
+    var  UpdateClass= Parse.Object.extend("ImageData");
+            var status="P";
+   var updateQuery = new UpdateClass();
+   updateQuery.id=itemID;
+   updateQuery.set("item_status",status);
+   updateQuery.save(null,{
+      success: function(updateQuery) {
+
+supersonic.logger.log("saved successfully");
+          
+ 
+      },
+      error: function(updateQuery,error) {
+        supersonic.ui.dialog.alert('Not Working!!');
       }
-      swipeID = -1;
+    });
 
-      var instructions = {
-                message: name,
-                buttonLabel: "Ok"
-            };
-
-      supersonic.ui.dialog.alert("Item removed:", instructions).then(function() {});
+ 
 
     }
 
