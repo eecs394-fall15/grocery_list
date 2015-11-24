4383a7bb718692039f44f544488e242af0049b2e
neha-2-r <rathi@u.northwestern.edu>
2015-11-23 20:57:39 -0600

    login working!

--- app/example/scripts/LoginController.js
+++ app/example/scripts/LoginController.js
@@ -7,25 +7,26 @@ angular
 
 	$scope.forward=function()
 	{
-		var loginName = $scope.name;
-		var loginEmail = $scope.email;
+		var loginName = $scope.name.toString();
+		var loginEmail = $scope.email.toString();
 
 		localStorage.setItem("loginName", loginName);
 		localStorage.setItem("loginEmail", loginEmail);
+
+		var loginClass = Parse.Object.extend("LoginData");
+
+		var loginObject = new loginClass();
+		loginObject.set("username", loginName);
+		loginObject.set("email", loginEmail);
+
+		loginObject.save();
 
 		var options = {
 		  animate: true
 		}
 
 		supersonic.ui.modal.hide(options);
-
-
 	}
-
-
-
-
-
   });
 
 
